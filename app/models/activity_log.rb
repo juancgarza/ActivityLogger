7386fb603efc8714e156d6d3905dd6e6ff095af4
Juan Carlos Garza <jcgarzar@gmail.com>
2017-06-14 07:55:53 -0500

    Final commit.

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -5,4 +5,14 @@ class ActivityLog < ApplicationRecord
   belongs_to :baby
   belongs_to :activity
 
+scope :in_progress, -> { where("duration is NULL")}
+
+scope :with_assistant_name, lambda { |assistant_name|
+  where(assistant: { name: assistant_name }).joins(:assistant)
+}
+scope :with_baby_name, lambda { |baby_name|
+  where(baby: { name: baby_name }).joins(:baby)
+}
+
+
 end
